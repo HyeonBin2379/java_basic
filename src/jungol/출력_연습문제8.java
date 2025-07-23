db4abb37496e8d95de14f048c6019ad07b168b55
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-23 17:22:01 +0900

    day01code_update

---
+++ src/jungol/출력_연습문제8.java
@@ -0,0 +1,12 @@
+package jungol;
+
+public class 출력_연습문제8 {
+
+    public static void main(String[] args) {
+        System.out.printf("%14s\n", "subject score");
+        System.out.println("==============");
+        System.out.printf("%8s%6d\n", "korean", 90);
+        System.out.printf("%8s%6d\n", "english", 100);
+        System.out.printf("%8s%6d\n", "computer", 80);
+    }
+}
