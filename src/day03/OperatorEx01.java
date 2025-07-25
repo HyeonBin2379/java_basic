d2ad05454a319f7a2d7d8a1db9159ae023a98cb7
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-25 17:44:19 +0900

    day03_04

--- src/day03/OperatorEx01.java
+++ src/day03/OperatorEx01.java
@@ -55,5 +55,48 @@ public class OperatorEx01 {
         z = ++x1 + y1++;    // x1이 14로 증가 -> x1+y1 = 22를 z에 대입 -> y1이 9로 증가
         System.out.println("z : " + z + " y1 : " + y1);
         System.out.println();
+
+
+        // 산술 연산자: 더하기, 빼기, 곱하기, 나누기, 나머지의 5가지
+        byte v1 = 10;
+        byte v2 = 4;
+        int v3 = 5;
+        long v4 = 10L;
+
+        // 피연산자의 모든 정수 타입 연산의 표준은 int 타입
+        // -> 모든 피연산자는 int 타입으로 자동변환 후 연산됨
+        int result4 = v1 + v2;
+        System.out.println(result4);
+
+        // 모든 피연산자들은 가장 크기가 큰 long 타입으로 promotion 연산
+        long result5 = v1+v2+-v4;
+        System.out.println(result5);
+
+        // 정수 간 연산 결과에서 소수점 이하 자릿수를 유지해야 할 경우, double 타입으로 강제변환 후 연산
+        double result6 = (double)v1/v2;
+        System.out.println(result6);
+
+        int result7 = v1 % v2;
+        System.out.println(result7);
+
+        if (v1 % v2 == 0) {
+            System.out.println("짝수 입니다.");
+        } else {
+            System.out.println("홀수 입니다.");
+        }
+
+        int apple = 1;
+        int totalPieces = apple*10;
+        int number = 7;
+
+        int result = totalPieces - number;
+        System.out.println("10조각에서 남은 조각 : " + result);
+        System.out.println("사과 1개에서 남은 양 : " + result/10.0);
+
+
+        // 정수 타입 피연산자끼리 나눗셈 수행 시, 몫에 해당하는 값만 출력
+        int k = 5;
+        int result8 = k/2;
+        System.out.println(result8);
     }
 }
---
+++ src/jungol/연산자/연산자_연습문제5.java
@@ -0,0 +1,21 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_연습문제5 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        boolean result1 = a == b;
+        boolean result2 = b == c;
+
+        System.out.print((result1 ? 1 : 0) + " ");
+        System.out.print((result2 ? 1 : 0) + " ");
+        System.out.print((!result1 ? 1 : 0) + " ");
+        System.out.print((!result2 ? 1 : 0) + " ");
+    }
+}
