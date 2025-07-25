b004257bacae87cf71e4d53632fb04bff06a1f2c
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-25 18:17:26 +0900

    day03_06

---
+++ src/jungol/연산자/연산자_형성평가4.java
@@ -0,0 +1,15 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_형성평가4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(++num1 + " " + num2--);
+        System.out.println(num1 + " " + num2);
+    }
+}
