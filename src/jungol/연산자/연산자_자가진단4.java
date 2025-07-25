13cc8a908f195f671c8918d09df3e6e8ac3da10f
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-25 18:05:37 +0900

    day03_05

---
+++ src/jungol/연산자/연산자_자가진단4.java
@@ -0,0 +1,15 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_자가진단4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = (a++)*(--b);
+
+        System.out.printf("%d %d %d\n", a, b, result);
+    }
+}
---
+++ src/jungol/연산자/연산자_자가진단8.java
@@ -0,0 +1,18 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_자가진단8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int result1 = (a > b && b >= c) || (a > c && c >= b) ? 1 : 0;
+        int result2 = (a == b && b == c) ? 1 : 0;
+
+        System.out.println(result1 + " " + result2);
+    }
+}
