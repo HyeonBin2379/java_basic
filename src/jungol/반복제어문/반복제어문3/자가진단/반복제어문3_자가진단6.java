c9bacfaef770d1b9841c2bf2940d73745abb7b19
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-28 17:12:13 +0900

    반복제어문 연습4

---
+++ src/jungol/반복제어문/반복제어문3/자가진단/반복제어문3_자가진단6.java
@@ -0,0 +1,19 @@
+package jungol.반복제어문.반복제어문3.자가진단;
+
+import java.util.Scanner;
+
+public class 반복제어문3_자가진단6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char ch = 'A';
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >= i; j--) {
+                System.out.print((ch++));
+            }
+            System.out.println();
+        }
+    }
+}
