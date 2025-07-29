53148019484bcd8e54273d69f27cf2affebfa8ff
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-29 17:30:08 +0900

    배열1 문제풀이

---
+++ src/jungol/배열/배열1/과제/배열1_연습문제6.java
@@ -0,0 +1,18 @@
+package jungol.배열.배열1.과제;
+
+import java.util.Scanner;
+
+public class 배열1_연습문제6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = 0;
+        int[] arr = new int[10];
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            max = Math.max(arr[i], max);
+        }
+        System.out.println(max);
+    }
+}
---
+++ src/jungol/배열/배열1/과제/배열1_연습문제8.java
@@ -0,0 +1,21 @@
+package jungol.배열.배열1.과제;
+
+import java.util.Scanner;
+
+public class 배열1_연습문제8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        double avg = (double)sum/10;
+        System.out.printf("총점 = %d\n", sum);
+        System.out.printf("평균 = %.1f\n", avg);
+    }
+}
