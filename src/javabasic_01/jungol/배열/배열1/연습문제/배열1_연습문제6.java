e4ba3086b50dc2f5aff26e0e262cc4963d8d7805
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-07-30 18:09:55 +0900

    코드 수정2

--- src/javabasic_01/jungol/배열/배열1/연습문제/배열1_연습문제6.java
+++ src/javabasic_01/jungol/배열/배열1/연습문제/배열1_연습문제6.java
@@ -1,18 +1,19 @@
 package javabasic_01.jungol.배열.배열1.연습문제;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class 배열1_연습문제6 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int max = 0;
         int[] arr = new int[10];
 
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
-            max = Math.max(arr[i], max);
         }
-        System.out.println(max);
+
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
     }
 }
