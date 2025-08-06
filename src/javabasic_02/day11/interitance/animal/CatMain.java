43c830a9f10b359c04696143f5300511a1d32867
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-08-06 12:02:13 +0900

    day11 상속2

---
+++ src/javabasic_02/day11/interitance/animal/CatMain.java
@@ -0,0 +1,10 @@
+package javabasic_02.day11.interitance.animal;
+
+public class CatMain {
+
+    public static void main(String[] args) {
+        Cat obj = new Cat();
+        obj.meow();
+        obj.eat();
+    }
+}
---
+++ src/javabasic_02/day11/interitance/calculator/Calculation.java
@@ -0,0 +1,14 @@
+package javabasic_02.day11.interitance.calculator;
+
+public class Calculation {
+
+    public void addition(int num1, int num2) {
+        int result = num1 + num2;
+        System.out.println("두 수의 합: " + result);
+    }
+
+    public void subtraction(int num1, int num2) {
+        int result = num1 - num2;
+        System.out.println("두 수의 뺄셈: " + result);
+    }
+}
